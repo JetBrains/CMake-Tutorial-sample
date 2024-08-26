a803b37aae2e13f6ef2a04793b73952d4b504fc8
Oleg.Zinovyev <oleg.zinovyev@jetbrains.com>
2024-08-26 09:05:16 +0300

    Fixes for tickets CPP-39491 and CPP-39492

--- calc_lib.h
+++ calc_lib.h
@@ -1,6 +1,6 @@
-#ifndef CMAKE_TESTAPP_CALC_H
-#define CMAKE_TESTAPP_CALC_H
+#ifndef CALC_LIB_H
+#define CALC_LIB_H
 
 int add_values (int a, int b);
 
-#endif //CMAKE_TESTAPP_CALC_H
+#endif //CALC_LIB_H
--- ctest/addvalues_zero.cpp
+++ ctest/addvalues_zero.cpp
@@ -1,9 +1,10 @@
 #include "assert_macro.h"
+#include "../calc_lib.h"
 
 int test_addvalues_zero() {
     int err_code = 0;
-    assertEqual (0+0 == 0);
-    assertEqual ((-5)+5 == 0);
+    assertEqual (add_values(0, 0) == 0);
+    assertEqual (add_values(-5, 5) == 0);
     return err_code;
 }
 
